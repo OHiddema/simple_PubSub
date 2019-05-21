db3731e72f9a9fb7e8392df8656ba4237ba7a73c
Oege Hiddema <oegehiddema@gmail.com>
2019-05-21 20:49:14 +0200

    Remove stadium class(redundant), add 8 athletes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// import PubSub from 'pubsub-js'
-// const PubSub = require('pubsub-js');
-
 class Runner {
-    constructor(bibNumber) {
+    constructor(bibNumber, name) {
+        this.name = name;
         this.bibNumber = bibNumber;
         this.lapTimes = [];
     }
@@ -10,34 +8,28 @@ class Runner {
     addLap() {
         let d = new Date();
         this.lapTimes.push(d);
-        PubSub.publish('Laptime', [this.bibNumber, d]);
+        PubSub.publish('Laptime', [this.name, this.bibNumber, d]);
     }
 }
 
-class Stadium {
-    constructor(name, location, capacity) {
-        this.name = name;
-        this.location = location;
-        this.capacity = capacity;
-        this.competitors = [];
-    }
-
-    createCompetitors(n) {
-        for (let i = 1; i <= n; i++) {
-            this.competitors.push(new Runner(i));
-        }
-    }
-}
-
-let myStadium = new Stadium('Olympisch Stadion', 'Amsterdam', 10000);
-myStadium.createCompetitors(8);
-// competitors [0..7]
-// console.log(myStadium.competitors[7].bibNumber);
+// create an array of 8 runners
+let athlete = [];
+athlete.push(new Runner(1, 'Nijel Amos'));
+athlete.push(new Runner(2, 'Emmanuel Kipkurui Korir'));
+athlete.push(new Runner(3, 'Donavan Brazier'));
+athlete.push(new Runner(4, 'Abubaker Haydar Abdalla'));
+athlete.push(new Runner(5, 'Jonathan Kitilit'));
+athlete.push(new Runner(6, 'Cornelius Tuwei'));
+athlete.push(new Runner(7, 'Adam Kszczot'));
+athlete.push(new Runner(8, 'Wyclife Kinyamal'));
 
 
 var mySubscriber = function (msg, arr) {
-    console.log(msg, arr[0], arr[1]);
+    console.log(msg, arr[0], arr[1], arr[2]);
 };
 
 var token = PubSub.subscribe('Laptime', mySubscriber);
-myStadium.competitors[0].addLap();
+
+for (let i=0; i< athlete.length; i++) {
+    athlete[i].addLap();
+}
